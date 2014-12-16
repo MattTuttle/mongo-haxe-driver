38a8c01fb5d1b581d4f45a5c72f4e26eff5e74cc
Jonas Malaco Filho <jonas@jonasmalaco.com>
2014-12-16 21:26:35 -0200

    Indentation fixes (use tabs, not spaces)

--- test/QueryTest.hx
+++ test/QueryTest.hx
@@ -3,30 +3,30 @@ import org.mongodb.*;
 @:publicFields class QueryTest extends haxe.unit.TestCase
 {
 
-    var cnx : Mongo;
-    var db : Database;
+	var cnx : Mongo;
+	var db : Database;
 
-    override function setup()
-    {
-        cnx = new Mongo();
-        db = cnx.queryTest;
-        db.users.drop();
-    }
+	override function setup()
+	{
+		cnx = new Mongo();
+		db = cnx.queryTest;
+		db.users.drop();
+	}
 
-    override function tearDown()
-    {
-        db.users.drop();
-        db = null;
-        cnx.close();
-    }
+	override function tearDown()
+	{
+		db.users.drop();
+		db = null;
+		cnx.close();
+	}
 
-    function testCriteria()
-    {
-        db.users.insert({ _id : 1, age : 10});
-        db.users.insert({ _id : 1, age : 20});
-        db.users.find({ age : { "$gt" : 18 } });
-        assertTrue(true);
-    }
+	function testCriteria()
+	{
+		db.users.insert({ _id : 1, age : 10});
+		db.users.insert({ _id : 1, age : 20});
+		db.users.find({ age : { "$gt" : 18 } });
+		assertTrue(true);
+	}
 
 }
 
