092c2a5b41b0b456f37760fed4e797b940e15f23
Pascal Peridont <pperidont@motion-twin.com>
2015-02-26 22:53:02 +0100

    Try to improve ObjectID generation: - Use bigEndian (mainly for timestamp at start) - start sequence counter with a random value - hash localhost name

--- org/bsonspec/ObjectID.hx
+++ org/bsonspec/ObjectID.hx
@@ -13,6 +13,9 @@ class ObjectID
 		{
 			// generate a new id
 			var out:BytesOutput = new BytesOutput();
+			
+			// Endianness: https://github.com/mongodb/mongo/blob/master/src/mongo/bson/oid.h
+			out.bigEndian = true;
 #if haxe3
 			out.writeInt32(Math.floor(Date.now().getTime() / 1000)); // seconds
 #else
@@ -37,11 +40,11 @@ class ObjectID
 	}
 
 	public var bytes(default, null):Bytes;
-	private static var sequence:Int = 0;
+	private static var sequence:Int = Std.random(0x1000000);
 
 	// machine host name
 #if (neko || php || cpp)
-	private static var machine:Bytes = Bytes.ofString(sys.net.Host.localhost());
+	private static var machine:Bytes = haxe.crypto.Md5.make(Bytes.ofString(sys.net.Host.localhost()));
 #else
 	private static var machine:Bytes = Bytes.ofString("flash");
 #end
